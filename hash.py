49530a1bc468aaf4bfdd79655dc52dc2be87ee5b
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-01-26 09:34:40 -0600

    first commit

---
+++ hash.py
@@ -0,0 +1,10 @@
+import redis
+from time import sleep
+
+client=redis.Redis(host='127.0.0.1',port=6379)
+
+client.hset('Hero','Name','Drow Ranger')
+client.hset('Hero','Health','600')
+client.hset('Hero','Mana','200')
+
+print(client.hgetall('Hero'))
